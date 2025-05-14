ea5d7f1b9a0328aac2b44aad2f573f03a2db5a56
Beatris <beatrisraj7@gmail.com>
2025-05-14 23:34:50 +0300

    formatPerson && dayOfTheWeek

--- intruduction.ts
+++ intruduction.ts
@@ -3,4 +3,22 @@ function evenSum(n1:number, n2:number, n3:number): void{
     console.log(res % 2 === 0);
     
 }
-evenSum(2, 2,3)
+
+function dayOfTheWeek(n:number):void{
+    enum DaysOfTheWeek{
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    console.log(DaysOfTheWeek[n] || 'error');
+    
+}
+
+function formatPerson(personData:[string, number]):string{
+    return `Hello, my name ${personData[0]} and my age is ${personData[1]}`
+}
+console.log(formatPerson(['Ivan', 20]));
--- out/intruduction.js
+++ out/intruduction.js
@@ -3,5 +3,21 @@ function evenSum(n1, n2, n3) {
     const res = n1 + n2 + n3;
     console.log(res % 2 === 0);
 }
-evenSum(2, 2, 3);
+function dayOfTheWeek(n) {
+    let DaysOfTheWeek;
+    (function (DaysOfTheWeek) {
+        DaysOfTheWeek[DaysOfTheWeek["Monday"] = 1] = "Monday";
+        DaysOfTheWeek[DaysOfTheWeek["Tuesday"] = 2] = "Tuesday";
+        DaysOfTheWeek[DaysOfTheWeek["Wednesday"] = 3] = "Wednesday";
+        DaysOfTheWeek[DaysOfTheWeek["Thursday"] = 4] = "Thursday";
+        DaysOfTheWeek[DaysOfTheWeek["Friday"] = 5] = "Friday";
+        DaysOfTheWeek[DaysOfTheWeek["Saturday"] = 6] = "Saturday";
+        DaysOfTheWeek[DaysOfTheWeek["Sunday"] = 7] = "Sunday";
+    })(DaysOfTheWeek || (DaysOfTheWeek = {}));
+    console.log(DaysOfTheWeek[n] || 'error');
+}
+function formatPerson(personData) {
+    return `Hello, my name ${personData[0]} and my age is ${personData[1]}`;
+}
+console.log(formatPerson(['Ivan', 20]));
 //# sourceMappingURL=intruduction.js.map
