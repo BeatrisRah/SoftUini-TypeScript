d1c522d59858483ef8a03c2c49496b300a92a34d
Beatris <beatrisraj7@gmail.com>
2025-05-14 23:19:57 +0300

    Init

---
+++ intruduction.ts
@@ -0,0 +1,6 @@
+function evenSum(n1:number, n2:number, n3:number): void{
+    const res = n1 + n2 + n3
+    console.log(res % 2 === 0);
+    
+}
+evenSum(2, 2,3)
---
+++ out/intruduction.js
@@ -0,0 +1,7 @@
+"use strict";
+function evenSum(n1, n2, n3) {
+    const res = n1 + n2 + n3;
+    console.log(res % 2 === 0);
+}
+evenSum(2, 2, 3);
+//# sourceMappingURL=intruduction.js.map
