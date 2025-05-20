e9f65fc263c9babee461a7d03b1eb5c862272578
Beatris <beatrisraj7@gmail.com>
2025-05-20 17:44:40 +0300

    operator

--- advanced-data-types.ts
+++ advanced-data-types.ts
@@ -11,7 +11,25 @@ function optionalMultiplier(n1?:isNumOrStr, n2?:isNumOrStr, n3?:isNumOrStr ): nu
 
 }
 
-console.log(optionalMultiplier());
+// console.log(optionalMultiplier());
+type operatorParam = [
+    string | number | string[],
+    'Index' | 'Length' | 'Add',
+    number
+]
+
+
+function operator(...args: operatorParam): number | string{
+    if(args[1] === 'Index'){
+        return (args[0] as string[] | string)[args[2]]
+    } else if(args[1] === 'Length'){
+        return (args[0] as string[] | string).length;
+    } 
+    return Number((args[0] as number)) + args[2]
+}
+
+console.log(operator('7', 'Add', 3 ));
+
 
 
 
